da239314ae7a80c7c8b7d51df1b32ab1584df043
tonghao.yuan <tonghao.yuan@csdental.com>
2021-07-23 12:13:52 +0800

    feat: file name with non-asii character

--- src/encoding/main.cpp
+++ src/encoding/main.cpp
@@ -3,6 +3,45 @@
 #include <iostream>
 #include <string>
 
+#ifdef _WIN32
+#include <Windows.h>
+
+std::string widechar_to_utf8(const std::wstring& wstr) {
+  std::string utf8_str;
+  int utf8_str_size =
+      WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, 0, 0, 0, 0);
+  if (utf8_str_size > 0) {
+    std::vector<char> buffer(utf8_str_size);
+    WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, &buffer[0], utf8_str_size,
+                        0, 0);
+    utf8_str.assign(buffer.begin(), buffer.end() - 1);
+  }
+  return utf8_str;
+}
+
+std::wstring utf8_to_widechar(const std::string& str) {
+  std::wstring wstr;
+  int wstr_size = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, 0, 0);
+  if (wstr_size > 0) {
+    std::vector<wchar_t> buffer(wstr_size);
+    MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, &buffer[0], wstr_size);
+    wstr.assign(buffer.begin(), buffer.end() - 1);
+  }
+  return wstr;
+}
+
+void test_filename() {
+  const auto fn = utf8_to_widechar("中文.txt");  // OK
+  const auto fn1 = std::wstring(L"中文1.txt");   // error
+  for (const auto name : {fn, fn1}) {
+    std::ofstream ofs(name);
+    ofs << "中文";
+    ofs.close();
+  }
+}
+
+#endif
+
 auto main(int argc, char** argv) -> int {
   std::string chinese = "你好世界";
   std::string english = "helloworld!";
@@ -13,8 +52,7 @@ auto main(int argc, char** argv) -> int {
   std::cout << chinese << " size is " << chinese_size << std::endl;
   std::cout << english << " size is " << english_size << std::endl;
 
-  std::filesystem::path path("中文.txt");
-  std::ofstream ofs(path);
-  ofs << chinese;
-  ofs << "中文";
+#ifdef WIN32
+  test_filename();
+#endif
 }
