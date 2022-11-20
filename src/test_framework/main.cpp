68387b1dab9359d1cdc11044ce1acef579fe6dfd
tonghao.yuan <yuantonghao@gmail.com>
2022-11-20 21:15:06 +0800

    feat: improve error message

--- src/test_framework/main.cpp
+++ src/test_framework/main.cpp
@@ -9,11 +9,16 @@ TEST_CASE(test_case2) {
   std::cout << __FUNCTION__ << '\n';
 }
 
+TEST_CASE(always_false) {
+  std::cout << __FUNCTION__ << '\n';
+  CHECK(false);
+}
+
 TEST_CASE(test_case3) {
   std::cout << __FUNCTION__ << '\n';
 }
 
 TEST_CASE(one_plus_two_is_four) {
   std::cout << __FUNCTION__ << '\n';
-  CHECK(1 + 2 == 3);
+  CHECK(1 + 2 == 4);
 }
--- src/test_framework/ut.hpp
+++ src/test_framework/ut.hpp
@@ -12,7 +12,10 @@ namespace internal {
 using unit_func_t = std::function<void()>;
 struct test_case {
   std::string_view name;
+  std::string_view file;
+  int line;
   unit_func_t func;
+  bool failed = false;
 };
 
 inline auto& get_cases() {
@@ -20,8 +23,8 @@ inline auto& get_cases() {
   return cases;
 }
 
-inline auto register_case(std::string_view name, unit_func_t&& func) {
-  get_cases().push_back(test_case{name, func});
+inline auto register_case(std::string_view name, std::string_view file, int line, unit_func_t&& func) {
+  get_cases().push_back(test_case{name, file, line, func});
   return true;
 }
 
@@ -30,40 +33,50 @@ inline int run_unit_test() {
   const auto size = get_cases().size();
   size_t failed = 0;
   size_t idx = 1;
-  for (const auto& test : get_cases()) {
-    std::cout << "run test case " << idx << "/" << size << ' ' << test.name << "...\n";
+  for (auto& test : get_cases()) {
+    std::cout << "\nrunning " << idx << "/" << size << ' ' << test.name << "...\n";
     try {
       test.func();
       std::cout << idx << "/" << size << ' ' << test.name << " passed\n";
     } catch (const std::exception& e) {
-      std::cout << idx << "/" << size << ' ' << test.name << " failed: " << e.what() << '\n';
+      test.failed = true;
+      std::cerr << idx << "/" << size << ' ' << test.name << " failed\n";
       ++failed;
     }
     ++idx;
   }
 
-  std::cout << "================test case run summary==================\n";
+  std::cout << "\n================test case run summary==================\n";
   if (failed == 0) {
     std::cout << "all case(s) passed\n";
   } else {
-    std::cout << failed << " case(s) failed\n";
+    std::cerr << failed << " case(s) failed:\n";
+    const auto size = get_cases().size();
+    size_t idx = 1;
+    for (const auto& test : get_cases()) {
+      if (test.failed) {
+        std::cerr << "  " << idx << '/' << size << ' ' << test.name << " at " << test.file << ':' << test.line << '\n';
+      }
+      ++idx;
+    }
   }
 
-  std::cout << "================test case run complete=================\n";
+  std::cout << "\n================test case run complete=================\n";
 
   return failed == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
 }  // namespace internal
 
-#define TEST_CASE(name)                                                      \
-  inline void name();                                                        \
-  inline static auto register_##name = internal::register_case(#name, name); \
+#define TEST_CASE(name)                                                                          \
+  inline void name();                                                                            \
+  inline static auto register_##name = internal::register_case(#name, __FILE__, __LINE__, name); \
   inline void name()
 
-#define CHECK(x)              \
-  if (!(x)) {                 \
-    throw std::exception(#x); \
+#define CHECK(x)                                                                          \
+  if (!(x)) {                                                                             \
+    std::cerr << "CHECK failed: " << #x << " at " << __FILE__ << ':' << __LINE__ << '\n'; \
+    throw std::exception(#x);                                                             \
   }
 
 #ifdef UT_MAIN
