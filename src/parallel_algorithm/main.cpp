14de0352aeea4a4ff02224a42ac46706651dd7f9
tonghao.yuan <yuantonghao@gmail.com>
2023-05-22 23:35:04 +0800

    feat: parallel for

--- src/parallel_algorithm/main.cpp
+++ src/parallel_algorithm/main.cpp
@@ -1,35 +1,51 @@
 #include <algorithm>
+#include <chrono>
+#include <cmath>
 #include <execution>
 #include <iostream>
 #include <numeric>
+#include <thread>
 #include <vector>
 
 #include "common/scope_timer.h"
 #include "parallel_algorithm/parallel_for.h"
 #include "parallel_algorithm/parallel_reduce.h"
 
+template <typename T>
+inline auto work(T v) {
+  return sqrt(v);
+};
+
 void test_parallel_for() {
-  constexpr size_t size = 1'000'000'000;
-  //   std::vector<int> numbers_serial(size);
-  std::vector<int> numbers_para(size);
-  std::vector<int> numbers_std_para(size);
+  constexpr size_t size = 10'000'000;
+  std::vector<int> numbers_serial(size, 0);
+  std::iota(numbers_serial.begin(), numbers_serial.end(), 0);
+  std::vector<int> numbers_para(numbers_serial);
+  std::vector<int> numbers_std_para(numbers_serial);
 
   {
     TIMER("parallel_for");
-    parallel_for(
-        0, size, [&numbers_para](int i) { numbers_para[i] = i * 2; }, 100'000'000);
+    parallel_for(0, size, [&numbers_para](int i) { numbers_para[i] *= work(i); });
   }
 
-  //   {
-  //     TIMER("serial");
-  //     for (int i = 0; i < size; ++i) {
-  //       numbers_serial[i] = i * 2;
-  //     }
-  //   }
+  {
+    TIMER("serial");
+    for (int i = 0; i < size; ++i) {
+      numbers_serial[i] *= work(i);
+    }
+  }
 
   {
     TIMER("std parallel");
-    std::for_each(std::execution::par, numbers_std_para.begin(), numbers_std_para.end(), [](int& i) { i = i * 2; });
+    std::for_each(std::execution::par, numbers_std_para.begin(), numbers_std_para.end(), [](int& i) { i *= work(i); });
+  }
+
+  if (numbers_para != numbers_serial) {
+    std::cout << "parallel for failed\n";
+  }
+
+  if (numbers_std_para != numbers_serial) {
+    std::cout << "std parallel for failed\n";
   }
 }
 
@@ -70,6 +86,7 @@ void test_parallel_reduce() {
 }
 
 int main(int argc, char** argv) {
-  test_parallel_reduce();
+  // test_parallel_reduce();
+  test_parallel_for();
   return 0;
 }
--- src/parallel_algorithm/parallel_for.h
+++ src/parallel_algorithm/parallel_for.h
@@ -1,37 +1,24 @@
 #pragma once
 
-#include <algorithm>
-#include <iostream>
-#include <thread>
-#include <utility>
+#include <future>
 #include <vector>
 
-#include "common/scope_timer.h"
+
 
 template <typename Func>
-void worker(size_t begin, size_t end, Func&& f, size_t chunk, size_t stride) {
-  FUNC_TIMER;
-  while (begin < end) {
-    size_t local_end = begin + chunk;
-    for (int i = 0; i < chunk; ++i) {
-      reducer(begin + i);
-    }
-    begin += stride;
-  }
-}
-
-template <typename Func>
-void parallel_for(size_t begin, size_t end, Func&& f, size_t chunk = 1000) {
-  size_t chunk_num = (end - begin + chunk - 1) / chunk;
+inline void parallel_for(size_t begin, size_t end, Func&& f) {
   size_t hardware_threads = std::thread::hardware_concurrency();
-  size_t worker_num = std::min(chunk_num, hardware_threads);
+  const auto chunk = (end - begin) / hardware_threads;
+  std::vector<std::future<void>> results;
 
-  std::cout << "worker num is " << worker_num << std::endl;
-
-  std::vector<std::jthread> workers;
-  const auto stride = chunk * worker_num;
-
-  for (size_t i = 0; i < worker_num; ++i) {
-    workers.emplace_back(worker<Func>, i * chunk, end, std::forward<Func&&>(f), chunk, stride);
+  for (size_t i = 0; i < end; i += chunk) {
+    results.emplace_back(std::async(
+        std::launch::async,
+        [f](size_t begin, size_t end) {
+          while (begin < end) {
+            f(begin++);
+          }
+        },
+        i, std::min(chunk + i, end)));
   }
 }
